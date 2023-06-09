f51d809573ffb7776d9ce7d2ca9c4e61b0f11fa7
Anzelika Maslovska <anzelikamaslovska@gmail.com>
2023-06-09 14:40:24 +0300

    Initial commit

---
+++ src/javaproject/JavaProject.java
@@ -0,0 +1,36 @@
+package javaproject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class JavaProject {
+
+	public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.print("Enter your name: ");
+            String name = reader.readLine();
+
+            System.out.print("Enter your age: ");
+            int age = Integer.parseInt(reader.readLine());
+
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading input.");
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid age input. Please enter a valid number.");
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                System.out.println("An error occurred while closing the reader.");
+                e.printStackTrace();
+            }
+        }
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author zuk12
+ *
+ */
+module JavaProject {
+}
