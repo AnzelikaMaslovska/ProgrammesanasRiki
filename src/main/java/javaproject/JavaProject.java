65e4ba9cdad5a91a763f6d6a6c238a6657336647
AnzelikaMaslovska <135897752+AnzelikaMaslovska@users.noreply.github.com>
2023-06-11 23:58:28 +0000

    Changes for project to work in Jenkins

--- src/main/java/javaproject/JavaProject.java
+++ src/main/java/javaproject/JavaProject.java
@@ -1,36 +1,23 @@
 package main.java.javaproject;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 
 public class JavaProject {
 
-	public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: JavaProject <name> <age>");
+            return;
+        }
 
+        String name = args[0];
+        int age;
         try {
-            System.out.print("Enter your name: ");
-            String name = reader.readLine();
-
-            System.out.print("Enter your age: ");
-            int age = Integer.parseInt(reader.readLine());
-
-            System.out.println("Name: " + name);
-            System.out.println("Age: " + age);
-        } catch (IOException e) {
-            System.out.println("An error occurred while reading input.");
-            e.printStackTrace();
+            age = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
             System.out.println("Invalid age input. Please enter a valid number.");
-        } finally {
-            try {
-                reader.close();
-            } catch (IOException e) {
-                System.out.println("An error occurred while closing the reader.");
-                e.printStackTrace();
-            }
+            return;
         }
-	}
 
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
 }
--- src/test/java/javaproject/JavaProjectTest.java
+++ src/test/java/javaproject/JavaProjectTest.java
@@ -1,56 +1,35 @@
 package main.java.javaproject;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JavaProjectTest {
-    private final InputStream originalInputStream = System.in;
-    private final PrintStream originalPrintStream = System.out;
-
-    private ByteArrayInputStream testInputStream;
-    private ByteArrayOutputStream testOutputStream;
-
-    @BeforeEach
-    public void setUp() {
-        testOutputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(testOutputStream));
-    }
-
-    @AfterEach
-    public void tearDown() {
-        System.setIn(originalInputStream);
-        System.setOut(originalPrintStream);
-    }
 
     @Test
     public void testMainWithValidInput() {
-        String input = "John\n25\n";
-        testInputStream = new ByteArrayInputStream(input.getBytes());
-        System.setIn(testInputStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
 
-        JavaProject.main(new String[]{});
+        String[] args = {"John", "25"};
+        JavaProject.main(args);
 
-        String expectedOutput = "Enter your name: Enter your age: Name: John\nAge: 25\n";
-        assertEquals(expectedOutput, testOutputStream.toString());
+        String expectedOutput = "Name: John\nAge: 25\n";
+        assertEquals(expectedOutput.trim(), outputStream.toString().trim());
     }
 
     @Test
     public void testMainWithInvalidAgeInput() {
-        String input = "Jane\ninvalid\n";
-        testInputStream = new ByteArrayInputStream(input.getBytes());
-        System.setIn(testInputStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
 
-        JavaProject.main(new String[]{});
+        String[] args = {"Jane", "invalid"};
+        JavaProject.main(args);
 
-        String expectedOutput = "Enter your name: Enter your age: Invalid age input. Please enter a valid number.\n";
-        assertEquals(expectedOutput, testOutputStream.toString());
+        String expectedOutput = "Invalid age input. Please enter a valid number.";
+        assertEquals(expectedOutput.trim(), outputStream.toString().trim());
     }
 }
